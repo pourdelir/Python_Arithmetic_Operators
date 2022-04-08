f3dfac423d17bdf203d7dd648ab9d389303c577c
SAEED <Saeid.poordelir@gmail.com>
2022-04-08 21:20:08 +0100

    Start Python Basic Math Solution.

---
+++ Python Arithmetic Operators/Python_Arithmetic_Operators.py
@@ -0,0 +1,16 @@
+#This is All Python Arithmetic Operators
+print("All Python Arithmetic Operators")
+print("= " *8)
+a,b =4,5 #Assign 2 variables in one line
+print("a=4 and b=5")
+print(f"a+4= {a+b}") 
+print(f"a-4= {a-b}")
+print(f"a/4= {a/b}")
+print(f"a*4= {a*b}")
+print(f"a**4= {a**b}" + " ** = ^ ")
+print(f"a%4= {a%b} (Integer Modulus)")
+
+
+
+
+
