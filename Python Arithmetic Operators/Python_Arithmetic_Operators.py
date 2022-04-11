519f81775ffd28f5c1c702f42169cf4e41b1cfbc
SAEED <Saeid.poordelir@gmail.com>
2022-04-11 18:40:31 +0100

    Add Logical Comparison to Arithma..

--- Python Arithmetic Operators/Python_Arithmetic_Operators.py
+++ Python Arithmetic Operators/Python_Arithmetic_Operators.py
@@ -3,14 +3,19 @@ print("All Python Arithmetic Operators")
 print("= " *8)
 a,b =4,5 #Assign 2 variables in one line
 print("a=4 and b=5")
-print(f"a+4= {a+b}") 
-print(f"a-4= {a-b}")
-print(f"a/4= {a/b}")
-print(f"a*4= {a*b}")
-print(f"a**4= {a**b}" + " ** = ^ ")
-print(f"a%4= {a%b} (Integer Modulus)")
-
-
-
+print(f"a+b= {a+b}") 
+print(f"a-b= {a-b}")
+print(f"a/b= {a/b}")
+print(f"a*b= {a*b}")
+print(f"a**b= {a**b}" + " ** = ^ ")
+print(f"a%b= {a%b} (Integer Modulus)")
+print(f"a//b= {a//b} ; b//a= {b//a} (Floor Division)\n")
+#This is All Python Arithmetic Operators
+print("= = =")
+print("a=4 and b=5")
+print(f"a and b= {a and b}")
+print(f"a or b= {a or b}")
+print(f"a = b?= {a == b}")
+print(f"a != b?= {a != b}")
 
 
